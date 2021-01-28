ff584c3b4814ca7b35e1682fd49d9c47f9c2af48
Gontorwon Borh <gborh@gmail.com>
2021-01-28 15:15:52 -0800

    rearranging static file

--- helper.py
+++ helper.py
@@ -19,12 +19,12 @@ def get_contact_image(name):
     g= d.get_gender(name)
     if g == "male":
         num=random.randrange(3, 11)
-        link= f"/static/img/{num}.png"
+        link= f"/static/assets/img/contacts/male/{num}.png"
     elif g== 'female':
         num=random.randrange(11, 21)
-        link= f"/static/img/{num}.png"
+        link= f"/static/assets/img/contacts/female/{num}.png"
     else:
         num=random.randrange(1, 3)
-        link= f"/static/img/{num}.png"
+        link= f"/static/assets/img/contacts/andy/{num}.png"
 
     return link
